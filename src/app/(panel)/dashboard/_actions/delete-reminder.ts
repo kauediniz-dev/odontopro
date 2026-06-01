dfa577e19fdff13c73eae1de2b6e6f0608558d5d
kauediniz-dev <dinizkaue26@gmail.com>
2026-06-01 19:11:43 -0300

    feat: add function for delete reminders

---
+++ src/app/(panel)/dashboard/_actions/delete-reminder.ts
@@ -0,0 +1,35 @@
+"use server";
+
+import prisma from "@/app/lib/prisma";
+import { revalidatePath } from "next/cache";
+import z from "zod";
+
+const formSchema = z.object({
+  reminderId: z.string().min(1, "O ID do lembrete é obrigatório"),
+});
+
+type FormSchema = z.infer<typeof formSchema>; // Define the type for the form data
+
+export async function deleteReminder(formData: FormSchema) {
+  const schema = formSchema.safeParse(formData); // Validate the form data against the schema
+
+  if (!schema.success) {
+    return { error: schema.error.issues[0].message };
+  }
+
+  try {
+    await prisma.reminder.delete({
+      // Delete the reminder from the database
+      where: {
+        id: schema.data.reminderId,
+      },
+    });
+
+    revalidatePath("/dashboard/reminders"); // Revalidate the path to update the UI after deletion
+
+    return { data: "Lembrete excluído com sucesso!" };
+  } catch (err) {
+    console.error("Error deleting reminder:", err);
+    return { error: "Erro ao excluir lembrete." };
+  }
+}
--- src/app/(panel)/dashboard/components/reminders/reminder-list.tsx.tsx
+++ src/app/(panel)/dashboard/components/reminders/reminder-list.tsx.tsx
@@ -5,11 +5,25 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { Reminder } from "@prisma/client";
 import { Plus, Trash } from "lucide-react";
+import { deleteReminder } from "../../_actions/delete-reminder";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
 
 interface ReminderListProps {
   reminder: Reminder[];
 }
 export function ReminderList({ reminder }: ReminderListProps) {
+  const router = useRouter();
+  async function handleDeleteReminder(Id: string) {
+    const response = await deleteReminder({ reminderId: Id });
+
+    if (response.error) {
+      toast.error(response.error);
+      return;
+    }
+    toast.success(response.data);
+    router.refresh();
+  }
   return (
     <div className="flex flex-col gap-3">
       <Card>
@@ -27,20 +41,23 @@ export function ReminderList({ reminder }: ReminderListProps) {
               Nenhum lembrete encontrado...
             </p>
           )}
-          {reminder.map((item) => (
-            <article
-              key={item.id}
-              className="flex flex-wrap flex-row items-center justify-between py-2 bg-yellow-100 mb-2 px-2 rounded-md"
-            >
-              <p className="text-sm lg:text-base">{item.description}</p>
-              <Button
-                className="bg-red-500 hover:bg-red-400 shadow-none rounded-full p-2"
-                size="sm"
+          <ScrollArea className="h-85 lg:max-h=[calc(100vh-15rem)] pr-0 w-full flex-1">
+            {reminder.map((item) => (
+              <article
+                key={item.id}
+                className="flex flex-wrap flex-row items-center justify-between py-2 bg-yellow-100 mb-2 px-2 rounded-md"
               >
-                <Trash className="w-4 h-4 text-white" />
-              </Button>
-            </article>
-          ))}
+                <p className="text-sm lg:text-base">{item.description}</p>
+                <Button
+                  className="bg-red-500 hover:bg-red-400 shadow-none rounded-full p-2"
+                  size="sm"
+                  onClick={() => handleDeleteReminder(item.id)}
+                >
+                  <Trash className="w-4 h-4 text-white" />
+                </Button>
+              </article>
+            ))}
+          </ScrollArea>
         </CardContent>
       </Card>
     </div>
