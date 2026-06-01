b41d84c98c1f9bd204f0b35eedd90d993a3ce654
kauediniz-dev <dinizkaue26@gmail.com>
2026-06-01 01:04:10 -0300

    fix: chance reminders folder

--- src/app/(panel)/dashboard/components/reminders.tsx
+++
@@ -1,7 +0,0 @@
-import { getReminders } from "../_data-access/get-reminders";
-
-export async function Reminders({ userId }: { userId: string }) {
-  const reminders = await getReminders({ userId: userId });
-  console.log(reminders);
-  return <h1>Reminders</h1>;
-}
