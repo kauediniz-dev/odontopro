c1f3204aa483c6e868a4b0ffe2e421a270c50587
kauediniz-dev <dinizkaue26@gmail.com>
2026-06-15 13:30:53 -0300

    fix: change route url

--- src/app/api/clinic/appointment/route.ts
+++ src/app/api/clinic/appointment/route.ts
@@ -32,10 +32,18 @@ export const GET = auth(async function GET(request) {
     );
   }
   try {
-    // Criar uma data formatada
+    // 1. Criar as datas usando Date.UTC para garantir que estamos no fuso 0
     const [year, month, day] = dateString.split("-").map(Number);
-    const startDate = new Date(year, month - 1, day, 0, 0, 0, 0);
-    const endDate = new Date(year, month - 1, day, 23, 59, 59, 999);
+
+    // Use Date.UTC para evitar que o fuso horário local interfira
+    const startDate = new Date(Date.UTC(year, month - 1, day, 0, 0, 0, 0));
+    const endDate = new Date(Date.UTC(year, month - 1, day, 23, 59, 59, 999));
+    console.log(
+      "Buscando entre:",
+      startDate.toISOString(),
+      "e",
+      endDate.toISOString(),
+    );
 
     const appointments = await prisma.appointment.findMany({
       where: {
