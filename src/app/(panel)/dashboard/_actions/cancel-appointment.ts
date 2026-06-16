368cab1096f51681472affcfb642c615da89c11b
kauediniz-dev <dinizkaue26@gmail.com>
2026-06-16 15:55:07 -0300

    feat: add cancel appointment in a dashboard

---
+++ src/app/(panel)/dashboard/_actions/cancel-appointment.ts
@@ -0,0 +1,41 @@
+"use server";
+
+import { auth } from "@/app/lib/auth";
+import prisma from "@/app/lib/prisma";
+import { revalidatePath } from "next/cache";
+import z from "zod";
+
+const formSchema = z.object({
+  appointmentId: z.string().min(1, "Necessario fornecer um agendamento"),
+});
+
+type FormSchema = z.infer<typeof formSchema>; // Define o tipo para os dados do formulário
+
+export async function cancelAppointment(formData: FormSchema) {
+  const schema = formSchema.safeParse(formData);
+
+  if (!schema.success) {
+    return { error: schema.error.issues[0].message };
+  }
+
+  const session = await auth();
+
+  if (!session?.user?.id) {
+    return { error: "Usuário não autenticado." };
+  }
+
+  try {
+    await prisma.appointment.delete({
+      where: {
+        id: formData.appointmentId,
+        userId: session?.user?.id,
+      },
+    });
+
+    revalidatePath("/dashboard");
+
+    return { data: "Agendamento cancelado com sucesso!" };
+  } catch (err) {
+    return { error: "Erro ao cancelar agendamento." };
+  }
+}
