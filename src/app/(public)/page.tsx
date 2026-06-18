644dc4f4c4dd763903f9ba2cfac87da8d1057044
kauediniz-dev <dinizkaue26@gmail.com>
2026-06-18 13:10:42 -0300

    feat: add revalidate

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -4,6 +4,8 @@ import { Hero } from "./_components/hero";
 import { Professionals } from "./_components/professionals";
 import { getProfessionals } from "./_data-access/get-professionals";
 
+export const revalidate = 120; // Revalidate every 2 minutes (se for 0 ela é renderizada dinamicamente)
+
 export default async function Home() {
   const professionals = await getProfessionals();
 
